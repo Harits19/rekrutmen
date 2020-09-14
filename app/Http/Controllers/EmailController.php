dcb144f98c28b83431d7ed0174d46158d5d51b49
Harits <harits.abdullah19@gmail.com>
2020-09-14 21:32:18 +0700

    data env

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -12,13 +12,16 @@ class EmailController extends Controller
     public function send_email()
     {
         # code...
-        $to_name = 'harits';
-        $to_email = 'harits.abdullah19@gmail.com';
-        $data = array('name' => "Ogbonna Vitalis(sender_name)", "body" => "A test mail");
-        Mail::send('mail', $data, function ($message) use ($to_name, $to_email) {
-            $message->to($to_email, $to_name)
-                ->subject('Laravel Test Mail');
-            $message->from('bentzie19@gmail.com', 'Test Mail');
-        });
+
+        for ($x = 0; $x <= 5; $x++) {
+            $to_name = 'harits';
+            $to_email = 'harits.abdullah19@gmail.com';
+            $data = array('name' => "Ogbonna Vitalis(sender_name)", "body" => "A test mail");
+            Mail::send('mail', $data, function ($message) use ($to_name, $to_email) {
+                $message->to($to_email, $to_name)
+                    ->subject('Laravel Test Mail');
+                $message->from('bentzie19@gmail.com', 'Test Mail');
+            });
+        }
     }
 }
